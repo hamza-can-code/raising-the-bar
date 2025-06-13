bb528404348a452da2a30d7f91ffbb46581c94d6
hamza-can-code <speedyhamza@outlook.com>
2025-06-13 08:08:25 +0000

    Improved landing page performance

--- client/scripts/landing-page.js
+++ client/scripts/landing-page.js
@@ -1,13 +1,13 @@
-import { initThemeToggle }        from "./modules/themeToggle.js";
-import { initFadeIn }             from "./modules/fadeIn.js";
+// import { initThemeToggle }        from "./modules/themeToggle.js";
+// import { initFadeIn }             from "./modules/fadeIn.js";
 import { initTestimonialsSlider } from "./modules/slider.js";
 import { initCookieBanner }       from "./modules/cookieBanner.js";
 import { initNavModal }           from "./modules/navModal.js";
 import { initModalLogin }         from "./modules/modalLogin.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-  initThemeToggle();
-  initFadeIn();
+  // initThemeToggle();
+  // initFadeIn();
   initTestimonialsSlider();
   initCookieBanner();
   initNavModal();
