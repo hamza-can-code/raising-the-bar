7116d82005ae141da478a922be8196fa0d761baa
hamza-can-code <speedyhamza@outlook.com>
2025-07-11 13:27:40 +0000

    Updated MongoDB env

--- server/db.js
+++ server/db.js
@@ -3,6 +3,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
+  console.log("▶️  Raw MONGO_URI:", process.env.MONGO_URI);
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
