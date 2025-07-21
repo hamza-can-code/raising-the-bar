94070350aa8314c9c0df0873aee967babd6b68b1
hamza-can-code <speedyhamza@outlook.com>
2025-07-21 09:15:08 +0000

    Updated Price Per Day

--- client/scripts/log-in-checkout.js
+++ client/scripts/log-in-checkout.js
@@ -284,7 +284,7 @@ function removeDiscountPricing() {
     // Hide the discounted price element
     priceSpecialDiscount.style.display = "none";
     // Set the cost to the full price value (£29.99) for the subscription
-    costPerDaySpecial.textContent = "£0.67";
+    costPerDaySpecial.textContent = "£1.67";
     // Calculate cost per day based on full price: 29.99/30
     if (currencyTagSpecial) currencyTagSpecial.style.display = "block";
   }
